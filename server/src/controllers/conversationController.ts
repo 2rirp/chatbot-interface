66e2d4da559dcbff68f74a55b9074d5586ac5093
Jônatas Gomes <jongaglima@gmail.com>
2023-07-27 08:24:26 -0300

    feat: Add route to get conversations date that will be stored into the select

---
+++ server/src/controllers/conversationController.ts
@@ -0,0 +1,22 @@
+import { Request, Response, NextFunction } from "express";
+import ErrorHandler from "../errors";
+import ConversationServices from "../services/conversationService";
+
+export default class ConversationController {
+  async getConversationsDates(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const dates = await ConversationServices.getAllConversationsDates();
+
+      res.status(201).json({
+        error: null,
+        data: dates,
+      });
+    } catch (error) {
+      next(error);
+    }
+  }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import UserController from "./controllers/userController";
 import BotUserController from "./controllers/botUserController";
 import MessageController from "./controllers/messageController";
+import ConversationController from "./controllers/conversationController";
 import authenticate from "./middlewares/authenticator";
 
 const router = Router();
@@ -9,6 +10,8 @@ const router = Router();
 const userController: UserController = new UserController();
 const botUserController: BotUserController = new BotUserController();
 const messageController: MessageController = new MessageController();
+const conversationController: ConversationController =
+  new ConversationController();
 
 router.post("/login", userController.login.bind(userController));
 
@@ -30,4 +33,9 @@ router.get(
   messageController.getMessagesByUserId.bind(messageController)
 );
 
+router.get(
+  "/conversations/dates",
+
+  conversationController.getConversationsDates.bind(conversationController)
+);
 export default router;
