1d2a2fbc3eef7075bcc13b22aa3ae7d92b5fd690
seitih <henrique.seiti1@gmail.com>
2023-08-10 22:00:16 -0300

    feat: dropdown menu (only on RealTimePage);

---
+++ client/src/components/dropdownMenu/dropdownMenu.tsx
@@ -0,0 +1,105 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import PersonAdd from '@mui/icons-material/PersonAdd';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import Logout from '@mui/icons-material/Logout';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+
+interface IProps {
+    handleRegister: () => void;
+    handleHistory: () => void;
+    handleLogout: () => Promise<void>;
+}
+
+export default function DropdownMenu(props: IProps) {
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+//   const handleSelect = () => {
+//     console.log('Oie')
+//   };
+  return (
+    <React.Fragment>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+        <Tooltip title="Opções">
+          <IconButton
+            onClick={handleClick}
+            size="small"
+            sx={{ ml: 2 }}
+            aria-controls={open ? 'account-menu' : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? 'true' : undefined}
+          >
+            <Avatar sx={{ width: 32, height: 32 }}><MoreVertIcon/></Avatar>
+          </IconButton>
+        </Tooltip>
+      </Box>
+      <Menu
+        anchorEl={anchorEl}
+        id="account-menu"
+        open={open}
+        onClose={handleClose}
+        onClick={handleClose}
+        PaperProps={{
+          elevation: 0,
+          sx: {
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            mt: 1.5,
+            '& .MuiAvatar-root': {
+              width: 32,
+              height: 32,
+              ml: -0.5,
+              mr: 1,
+            },
+            '&:before': {
+              content: '""',
+              display: 'block',
+              position: 'absolute',
+              top: 0,
+              right: 14,
+              width: 10,
+              height: 10,
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0,
+            },
+          },
+        }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+      >        
+        <MenuItem onClick={props.handleRegister}>
+          <ListItemIcon>
+            <PersonAdd fontSize="small" />
+          </ListItemIcon>
+          Registrar usuário
+        </MenuItem>
+        <MenuItem onClick={props.handleHistory}>
+          <ListItemIcon>
+            <EventNoteIcon fontSize="small" />
+          </ListItemIcon>
+          Histórico de atendimentos
+        </MenuItem>
+        <MenuItem onClick={props.handleLogout}>
+          <ListItemIcon>
+            <Logout fontSize="small" />
+          </ListItemIcon>
+          Deslogar
+        </MenuItem>
+      </Menu>
+    </React.Fragment>
+  );
+}
