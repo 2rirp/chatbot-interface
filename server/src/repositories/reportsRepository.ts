96c5bb0f9b3a012d145c728506c4da155800c7b9
Jônatas Gomes <jongaglima@gmail.com>
2023-09-25 10:30:56 -0300

    fix: Fix query returning lots of repeated rows

--- server/src/repositories/reportsRepository.ts
+++ server/src/repositories/reportsRepository.ts
@@ -116,17 +116,23 @@ ORDER BY data;`
 
   async getAllUsersByDate(date: string) {
     try {
-      const queryText = `SELECT TO_CHAR(c.created_at, 'HH24:MI') AS date, c.id, c.user_id, c.status,
-      CASE WHEN u.link_certidao IS NOT NULL and link_certidao <> '' THEN TRUE ELSE FALSE END AS emitida
+      const queryText = `SELECT TO_CHAR(c.created_at, 'HH24:MI') AS date,
+      c.id,
+      c.user_id,
+      c.status,
+     (CASE WHEN r.link_certidao IS NOT NULL AND r.link_certidao <> '' THEN TRUE ELSE FALSE END) AS emitida
       FROM conversations c
-      LEFT JOIN registration u ON c.user_id = u.user_id
-      WHERE c.created_at >= $1::timestamp
-      AND c.created_at < ($1::timestamp + INTERVAL '1 day')
-      ORDER BY c.created_at;`;
+      LEFT JOIN (
+      SELECT user_id, link_certidao, conversation_id FROM registration
+      GROUP BY user_id, link_certidao, conversation_id
+) r ON c.user_id = r.user_id AND c.id = r.conversation_id
+WHERE c.created_at >= $1::timestamp
+AND c.created_at < ($1::timestamp + INTERVAL '1 day')
+ORDER BY c.created_at;`;
       const result = await this.db.pool.query(queryText, [date]);
       return result.rows;
     } catch (error) {
-      console.error('failed to get all user reports: ', error)
+      console.error("failed to get all user reports: ", error);
       throw error;
     }
   }
