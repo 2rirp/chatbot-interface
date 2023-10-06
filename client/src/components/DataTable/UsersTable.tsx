512386fc085688840b1789f958f5ae462d398b72
Jônatas Gomes <jongaglima@gmail.com>
2023-10-06 16:19:11 -0300

    style: Format code using Prettier

--- client/src/components/DataTable/UsersTable.tsx
+++ client/src/components/DataTable/UsersTable.tsx
@@ -1,11 +1,12 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import PhoneNumberFormatter from "../phoneNumberFormatter/PhoneNumberFormatter";
 
 interface IReportUsers {
   id: number;
@@ -16,57 +17,58 @@ interface IReportUsers {
 }
 
 interface IProps {
-    fetchedData : IReportUsers[];
+  fetchedData: IReportUsers[];
 }
 
-function createData(id: number, date : string, user_id : string, status : string, emitida: boolean | string) {
+function createData(
+  id: number,
+  date: string,
+  user_id: string,
+  status: string,
+  emitida: boolean | string
+) {
   return { id, date, user_id, status, emitida };
 }
 
 export default function UsersTable(props: IProps) {
-    const rows = props.fetchedData.map((item) => {
-      switch(item.status){
-        case "talking_to_attendant":
-          item.status = "Falando com atendente"
-          break;
-        case "active":
-          item.status = "Em andamento"
-          break;
-        case "inactive":
-          item.status = "Conversa encerrada"
-          break;
-        case "waiting_payment":
-          item.status = "Aguardando pagamento"
-          break;
-        case "confirmed_payment":
-          item.status = "Pagamento confirmado"
-          break;
-        default:
-          item.status;
-      }
-      if(item.emitida === true)
-        item.emitida = "Sim";
-      else if(item.emitida === false)
-        item.emitida = "Não"
-        
-      return createData(
-          item.id,
-          item.date || '',
-          item.user_id || "0",
-          item.status || "0",
-          item.emitida
-      );
-    });
+  const rows = props.fetchedData.map((item) => {
+    switch (item.status) {
+      case "talking_to_attendant":
+        item.status = "Falando com atendente";
+        break;
+      case "active":
+        item.status = "Em andamento";
+        break;
+      case "inactive":
+        item.status = "Conversa encerrada";
+        break;
+      case "waiting_payment":
+        item.status = "Aguardando pagamento";
+        break;
+      case "confirmed_payment":
+        item.status = "Pagamento confirmado";
+        break;
+      default:
+        item.status;
+    }
+    if (item.emitida === true) item.emitida = "Sim";
+    else if (item.emitida === false) item.emitida = "Não";
 
-    
-  
+    return createData(
+      item.id,
+      item.date || "",
+      item.user_id || "0",
+      item.status || "0",
+      item.emitida
+    );
+  });
 
   return (
     <TableContainer component={Paper}>
-      <Table sx={{minWidth: 650, fontSize: "30px"}} aria-label="simple table">
+      <Table sx={{ minWidth: 650, fontSize: "30px" }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell >Conversa (id)</TableCell>
+            <TableCell>Conversa (id)</TableCell>
             <TableCell align="right">Horário</TableCell>
             <TableCell align="right">Usuário</TableCell>
             <TableCell align="right">Status</TableCell>
@@ -77,7 +79,7 @@ export default function UsersTable(props: IProps) {
           {rows.map((row) => (
             <TableRow
               key={row.id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell component="th" scope="row">
                 {row.id}
